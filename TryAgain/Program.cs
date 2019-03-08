8e50272e1e87cd48da7b39c5b2aebfbfb0c1bcca
Joel-Crunk <joel.crunk@outlook.com>
2019-03-08 13:30:35 -0500

    everything but trimming list

--- TryAgain/Program.cs
+++ TryAgain/Program.cs
@@ -8,23 +8,16 @@ using System.IO;
 namespace TryAgain
 {
     class Program
-    {
-        
-
+    {        
         static void Main(string[] args)
         {
             const char rs = '\x1e';
             bool menu = false;
             var path = @"C:\Users\Joel.Crunk\source\repos\TryAgain\TryAgain\Testingfile.txt";
-            //var task = new List<string>();
             bool EXIT = false;
             List<Task> task = new List<Task>();
-            //string path = @"C:\Users\Joel.Crunk\source\repos\TaskTrackingAppVersion2.0\TaskTrackingAppVersion2.0\TaskList1.txt";
             using (StreamReader sr = new StreamReader(path))
             {
-                //string line;
-                //    // Read and display lines from the file until the end of 
-                //    // the file is reached.
                 while (!sr.EndOfStream)
                 {
                     string newTask = sr.ReadLine();
@@ -41,46 +34,159 @@ namespace TryAgain
                     }
                 }
             }
-            //var task = new List<Task>();
-
             Console.WriteLine($"  { task.Count} tasks in your list");
-            void PrintListAction()
-            {
 
+            void displayPage(int current = 1)
+            {
+                int lowerLimit = 25 * (current - 1);
+                int upperLimit = ((lowerLimit + 25) > task.Count) ? task.Count : lowerLimit + 25;
+
+                for (int i = lowerLimit; i < upperLimit; i++)
+                {
+                    if (task[i].Complete)
+                    {
+                        string str = $"\n  {i + 1}. {task[i].Details}";
+                        str = str.Substring(0, str.Length - 1);
+
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.WriteLine(str);
+                        Console.ResetColor();
+                      
+                    }
+                    else 
+                    {
+                        Console.Write($"\n  {i + 1}. {task[i].Details}\n");
+                      
+                    }
+                }
             }
 
             void PrintList()
             {
-                for (int x = 0; x < task.Count; x++)
+                
+                int currentPage = 1;
+                int numPages = (int) Math.Ceiling(task.Count / 25.0);
+
+                if (numPages <= 1)
                 {
-                    if (!task[x].Complete)
-                    {
-                        for (int i = x; i < task.Count; i++)
-                        {
-                           
-                            if (task[i].Complete)
-                            {
-
-                                string str;
-                                str = $"\n  {i + 1}. {task[i].Details}";
-                                str = str.Substring(0, str.Length - 1);
-                                Console.ForegroundColor = ConsoleColor.DarkGray;
-                                Console.WriteLine(str);
-                                Console.ResetColor();
-                                x = task.Count + 1;
-                            }
-                            else
-                            {
-                                Console.WriteLine($"\n  {i + 1}. {task[i].Details}\n");
-                                x = task.Count + 1;
-                            }                            
-                        }
-                        
-                    }
-                    
+                    displayPage();
                 }
-            }
+                else
+                {
+                    do
+                    {
+                        displayPage(currentPage);
+                        //display page nav menu
+                        Console.Write($"\n  0) EXIT\n  1) NEXT PAGE\n  ");
+                        int pageMenu = int.Parse(Console.ReadLine());
+                        if (pageMenu == 1)
+                        {
+                            currentPage++;
+                            if(currentPage > numPages)
+                            {
+                                currentPage = 1;
+                            }
+                        }
+                        else if (pageMenu == 0)
+                        {
+                            break;
+                        }
+                    } while (true);
+                   
+                }
 
+                //for (int x = 0; x < task.Count; x++)
+                //{
+                //    counter++;
+                    
+                    
+                //    if (!task[x].Complete)
+                //    {
+                //        int co = 0;
+                //         co = counter + 24;
+                //        for (int i = x; i < co; i++)
+                //        {
+                //            if (task[i].Complete)
+                //            {
+                //                string str;
+                //                str = $"  {i + 1}. {task[i].Details}";
+                //                str = str.Substring(0, str.Length - 1);
+                //                Console.ForegroundColor = ConsoleColor.DarkGray;
+                //                Console.WriteLine(str);
+                //                Console.ResetColor();
+                //                x = task.Count;
+                //            }
+                //            else if (!task[i].Complete)
+                //            {
+                //                Console.WriteLine($"\n  {i + 1}. {task[i].Details}\n");
+                //                x = task.Count;
+                //            }                            
+                //        }
+                //        int z = 0;
+                //        z = counter + 49;
+                //        Console.Write($"  0) EXIT\n  1) NEXT PAGE\n  ");
+                //        int put = int.Parse(Console.ReadLine());
+                //        if (put == 1)
+                //        {
+                           
+                //            for (int i = co; i < z; i++)
+                //            {
+                //                if (task[i].Complete)
+                //                {
+                //                    string str;
+                //                    str = $"\n  {i + 1}. {task[i].Details}";
+                //                    str = str.Substring(0, str.Length - 1);
+                //                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                //                    Console.WriteLine(str);
+                //                    Console.ResetColor();  
+                //                }
+                //                else
+                //                {
+                //                    Console.WriteLine($"\n  {i + 1}. {task[i].Details}\n");      
+                //                }
+                //            }
+                            
+                //        }
+                            /*
+                            int aa = z + 25;
+                            for (int i = z; i < aa; i++)
+                            {
+                                if (task[z].Complete)
+                                {
+                                    string str;
+                                    str = $"\n  {z + 1}. {task[z].Details}";
+                                    str = str.Substring(0, str.Length - 1);
+                                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                                    Console.WriteLine(str);
+                                    Console.ResetColor();                                  
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"\n  {z + 1}. {task[z].Details}\n");                                 
+                                }
+                            }
+                            int bb = aa + 25;
+                            for (int i = aa; i < bb; i++)
+                            {
+                                if (task[aa].Complete)
+                                {
+                                    string str;
+                                    str = $"\n  {aa + 1}. {task[aa].Details}";
+                                    str = str.Substring(0, str.Length - 1);
+                                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                                    Console.WriteLine(str);
+                                    Console.ResetColor();
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"\n  {aa + 1}. {task[aa].Details}\n");
+                                }
+                            }
+                            */
+                        //}
+                        //  /  
+                    //}
+            }
             do
             {
                 Console.WriteLine(" \n  MAKE A SELECTION: \n\n");
@@ -89,10 +195,7 @@ namespace TryAgain
                 Console.WriteLine("  Select 2. to VIEW.");
                 Console.Write("  Select 3. to MODIFY.\n\n  ");
 
-
                 string input = Console.ReadLine();
-
-
 
                 if (input == "0")
                 {
@@ -102,12 +205,8 @@ namespace TryAgain
                 {
                     do
                     {
-
-
-
-
                         Console.Clear();
-                        PrintList();
+                        menu = false;
                         Console.Write("\n\n  Menu = 0\n  or\n  Enter new task: \n\n  ");
                         string newTask = Console.ReadLine();
 
@@ -121,19 +220,12 @@ namespace TryAgain
                             task.Add(new Task(newTask));
                         }
 
-
-
                     } while (!menu);
-
                 }
                 else if (input == "2")
                 {
                     Console.Clear();
                     PrintList();
-                    
-                    //Console.WriteLine();
-
-
 
                 }
                 else if (input == "3")
@@ -146,7 +238,6 @@ namespace TryAgain
                     var mod = int.Parse(lin);
                     if (mod <= task.Count)
                     {
-                        //Console.Clear();
                         Console.WriteLine($"\n  1) MARK as complete.\n");
                         Console.WriteLine($"  2) DELETE\n");
                         Console.Write($"  3) Return to Main Menu\n\n  ");
@@ -174,47 +265,8 @@ namespace TryAgain
                     else
                     {
                         Console.WriteLine("\n  Invalid input! ");
-                    }
-
-  
-                    
-                    //string Replace(string oldValue, string newValue);
-                    //task.
-                    //for (int i = 1; i < mod; i++)
-                    //{
-                    //    Console.WriteLine(task[mod].Details);
-                    //}
-
-
-
-                    //using (StreamReader reader = new StreamReader(path))
-                    //{
-                    //    for (int i = 1; i <= mod; ++i)
-                    //        lin = reader.ReadLine();
-                    //}
-                    //int line_number = 1;
-                    //string line = null;
-
-                    ////string linetoWrite = null;
-                    //using (StreamReader reader = new StreamReader(path))
-                    //using (StreamWriter writer = new StreamWriter(tempFile))
-                    //    while ((line = reader.ReadLine()) != null)
-                    //    {
-                    //        if (line_number == mod)
-                    //        {
-                    //            writer.WriteLine(lin);
-                    //        }
-                    //        else
-                    //        {
-                    //            writer.WriteLine(line);
-                    //        }
-                    //        line_number++;
-                    //    }
-
-                }
-               
-
-
+                    }                    
+                }               
             } while (!EXIT);
 
             using (StreamWriter sw = new StreamWriter(path))
@@ -222,11 +274,8 @@ namespace TryAgain
                 foreach (var newTask in task )
                 {
                     sw.WriteLine( newTask.Details);
-
                 }
             }
-
-            // File.WriteAllLines(path, task.ToArray());
         }
     }
 }
